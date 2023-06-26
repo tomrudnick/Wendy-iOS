fac4ea09af00ab271b596f2ced2884bb107e825a
Tom Rudnick <tom@rudnick.ch>
2023-06-26 11:16:02 +0200

    convert to Package.swift

--- Wendy/Classes/DB/Manager/CoreDataManager.swift
+++ Wendy/Classes/DB/Manager/CoreDataManager.swift
@@ -28,7 +28,7 @@ internal class CoreDataManager {
 
     private lazy var managedObjectModel: NSManagedObjectModel = {
         // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = Bundle.frameworkUrlForWendyFramework().url(forResource: "Wendy", withExtension: "momd")!
+        let modelURL = Bundle.module.url(forResource: "Wendy", withExtension: "momd")!
         return NSManagedObjectModel(contentsOf: modelURL)!
     }()
 
